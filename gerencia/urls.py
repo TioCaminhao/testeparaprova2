1e002f662ab2845d3da84a8f6b5b4a28e110feae
TioCaminhao <savio.v@escolar.ifrn.edu.br>
2024-12-15 15:56:12 -0300

    feito novamente

---
+++ gerencia/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import*
+
+app_name = 'gerencia'
+
+urlpatterns = [
+    path('', inicio_gerencia, name='gerencia_inicial'),
+    path('noticias/', listagem_noticia, name='listagem_noticia'),
+    path('noticias/cadastro', cadastrar_noticia, name='cadastro_noticia'),
+    path('noticias/editar/<int:id>', editar_noticia, name='editar_noticia'),
+    path('categorias/', listagem_categoria, name='listagem_categoria'),
+    path('categorias/cadastro', cadastrar_categoria, name='cadastro_categoria'),
+    path('categorias/editar/<int:id>', editar_categoria, name='editar_categoria'),
+    path('categorias/deletar/<int:id>', excluir_categoria, name='excluir_categoria'),
+    # Add your URL patterns here
+]
